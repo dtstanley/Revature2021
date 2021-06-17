9920a79afe1f1cec0d1ee47c53729e8f74a27442
DT <deep7dt@gmail.com>
2021-06-17 15:14:26 -0400

    first commit Day9 lab

---
+++ PuttingTogetherDay9/src/com/revature/Car.java
@@ -0,0 +1,59 @@
+package com.revature;
+
+public class Car implements Comparable<Car>{
+
+	String make;
+	String model;
+	int year;
+	
+	
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+
+	//}
+	 
+	public int compareTo(Car other) {
+		return this.year - other.year;
+	}
+
+	//getters
+	public String getMake() {
+		return make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	//setters
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public interface Comparable<T>{
+		int comparareTo (T other);
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Car [make=" + make + ", model=" + model + ", year=" + year + "]";
+	}
+	
+	
+	
+	
+}
---
+++ PuttingTogetherDay9/src/com/revature/CarSort.java
@@ -0,0 +1,16 @@
+package com.revature;
+
+import java.util.ArrayList;
+
+public class CarSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		ArrayList<Car> Collection = new ArrayList<Car>();
+		
+		Collection.add("Monarch");
+	}
+
+}
